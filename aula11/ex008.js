88569d3f11f11c6e8291c57630d176d5dee2d9df
Silas Miquéias <112900164+SilasMiqueias@users.noreply.github.com>
2022-10-31 15:42:50 -0300

    .

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é de ${vel}Km/h.`)
+if(vel > 60){
+    console.log('Voçe ultrapassou o limite de velocidade, MULTADO!')
+}else{
+    console.log('Obrigado por respeitar as leis de transito!')
+}
+console.log('Dirigja sempre com o cinto de segurança!')
