1f6026199bb0d01946a81b11c316ac4a8a9eed27
Silas Miquéias <112900164+SilasMiqueias@users.noreply.github.com>
2022-11-08 20:09:28 -0300

    finalizado

--- aula15/valoresnatela.js
+++ aula15/valoresnatela.js
@@ -1,4 +1,7 @@
-let valores = [8,9,6,3,2,4,7,1]
+/*let valores = [8,9,6,3,2,4,7,1]
 for(let pos=0; pos<valores.length; pos++){
-    console.log(`A posição ${pos} tem o número ${valores}`)
+    console.log(`A posição ${pos} tem o número ${valores[pos]}`)
+}*/
+for(let pos in valores){
+    console.log(`A posição ${pos} tem o número ${valores[pos]}`)
 }
